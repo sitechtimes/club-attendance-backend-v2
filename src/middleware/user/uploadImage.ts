a4a0eccaec3bbdc5b833bbf5f386bbd0ca1e6c18
Johnson Wang <johnsonwang529@gmail.com>
2023-07-25 12:08:00 -0400

    can now upload images to club folders

--- src/middleware/user/uploadImage.ts
+++ src/middleware/user/uploadImage.ts
@@ -1,29 +1,69 @@
-import { Request, Response, NextFunction } from 'express'
-import { service } from '../../app'
-import { Readable } from 'stream'
+import { Request, Response, NextFunction } from "express";
+import { service } from "../../app";
+import { Readable } from "stream";
+
+export const uploadImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  const year = req.body.year
+  const clubName = req.body.clubName
+
+  let result = await service.files
+    .list({
+      q: "mimeType='application/vnd.google-apps.folder'",
+      fields: "nextPageToken, files(id, name)",
+      spaces: "drive",
+    })
+    .catch((error) => console.log(error));
+  let folder = result.data.files;
+  const selectedFolder = folder?.filter((folder) => folder.name === year);
+  const folderId = selectedFolder[0].id;
+
+    //searches in the year folder for the club folder
+  const clubResult = await service.files.list({
+    q: `name = '${clubName}' and '${folderId}' in parents`,
+    fields: "nextPageToken, files(id, name)",
+  })
 
 
-export const uploadImage = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const clubName = req.body.clubName;
-        console.log(req.file)
 
-        const file = await service.files.create({
-            requestBody: {
-                name: req.file?.originalname,
-                parents: [`${process.env.CLUB_ATTENDANCE_FOLDER_ID}`],
-              },
-              media: {
-                mimeType: req.file?.mimetype,
-                body: Readable.from([req.file?.buffer]),
-              },
-        });
-        console.log('File Id:', file.data.id);
-        // return file.data.id;
-        res.json({ message: 'File uploaded successfully!' })
 
-      } catch (err) {
-       res.json(err)
-      }
-    
-}
+  const selectedClubId = clubResult.data.files[0].id;
+  const selectedClubName = clubResult.data.files[0].name;
+
+    //searches in the club folder for the attendance folder
+  const selectedClubResult = await service.files.list({
+    q: `'${selectedClubId}' in parents`,
+    fields: "nextPageToken, files(id, name)",
+  })
+
+
+  const attendanceFolder = selectedClubResult.data.files?.filter((file) => file.name?.includes("Attendence Photos"))
+
+
+  const attendanceFolderId = attendanceFolder[0].id
+
+
+  try {
+    console.log(req.file);
+
+    const file = await service.files.create({
+      requestBody: {
+        name: req.file?.originalname,
+        parents: [`${attendanceFolderId}`],
+      },
+      media: {
+        mimeType: req.file?.mimetype,
+        body: Readable.from([req.file?.buffer]),
+      },
+    });
+    console.log("File Id:", file.data.id);
+    // return file.data.id;
+    res.json({ message: "File uploaded successfully!" });
+  } catch (err) {
+    res.json(err);
+  }
+};
