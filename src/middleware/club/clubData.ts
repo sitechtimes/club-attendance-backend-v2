ac39fe87e170ec7cd57ce32e15a08fe0586f0c8e
Johnson Wang <johnsonwang529@gmail.com>
2023-07-27 16:07:06 -0400

    uoh

--- src/middleware/club/clubData.ts
+++ src/middleware/club/clubData.ts
@@ -36,3 +36,48 @@ export const getClubData = async (req: Request, res: Response, next: NextFunctio
         res.json(error)
     }
 }
+
+//add club
+export const addClubData = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const clubName = req.body.clubName
+        const clubAdvisor = req.body.clubAdvisor
+        const clubPresident = req.body.clubPresident
+        const frequency = req.body.frequency
+        const day = req.body.day
+        const room = req.body.room
+        const activtyType = req.body.activityType
+        const status = req.body.status
+        const email = req.body.email
+
+        await clubNameDoc.loadInfo()
+        const clubNameSheet = clubNameDoc.sheetsById[0]
+        await clubNameSheet.addRow({
+            "Club Name": clubName,
+            "Club Advisor": clubAdvisor,
+            "Club President(s)": clubPresident,
+            Frequency: frequency,
+            Day: day,
+            Room: room,
+            "Activity Type": activtyType,
+            Status: "APPROVED",
+            Email: email
+        })
+
+        res.json(`${clubName} has been added!`)
+
+    } catch (error) {
+        res.json(error)
+    }
+}
+
+
+//delete club
+export const deleteClubData = async (req: Request, res: Response, next: NextFunction) => {
+try{
+    res.json("sdcjsndnc")
+    } catch (error) {
+        res.json(error)
+    }
+}
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { oauth2, oauth2callback } from '../middleware/auth';
 import { createUserSheet } from '../middleware/user/userData';
-import { getClubData } from '../middleware/club/clubData';
+import { getClubData, addClubData, deleteClubData } from '../middleware/club/clubData';
 import { createClubTemplate } from '../middleware/scripts/drive';
 import { uploadImage } from '../middleware/user/uploadImage';
 import { upload } from '../middleware/user/multer';
@@ -26,10 +26,12 @@ router.post("/createClubTemplate",verifyAdmin, createClubTemplate)
 // router.post("/createClubMeta", createClubMeta)
 router.post("/uploadImage", upload.array("image"), uploadImage)
 router.post("/addClubMeeting", addClubMeeting)
+router.post("/addClub", verifyAdmin, addClubData)
 
 router.patch("/updateAttendance", updateAttendance)
 // router.patch("/changeMeta", changeMeta)
 
 router.delete("/deleteClubMeeting", deleteClubMeeting)
+router.delete("/deleteClub", deleteClubData)
 
 export { router };
