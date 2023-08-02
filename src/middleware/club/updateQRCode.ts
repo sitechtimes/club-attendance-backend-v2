665e1b59a751bb01b332ca3019e6332320b8cce8
Harvey Jiang <harveyjiang11@gmail.com>
2023-08-02 08:25:19 -0400

    wa

--- src/middleware/club/updateQRCode.ts
+++ src/middleware/club/updateQRCode.ts
@@ -2,99 +2,106 @@ import { Request, Response, NextFunction } from "express";
 import { serviceAccountAuth, service } from "../../app";
 import { GoogleSpreadsheet, GoogleSpreadsheetCell } from "google-spreadsheet";
 import { google } from "googleapis";
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
 import { clubNameDoc } from "../../app";
 import QRCode, { create } from "qrcode";
 import { upload } from "../user/multer";
 import { cloudbuild } from "googleapis/build/src/apis/cloudbuild";
 import fs from "fs";
-import { Readable } from 'stream'
+import { Readable } from "stream";
 
 interface metaData {
-    club_name: string;
-    club_parent_id: string;
-  }
+  club_name: string;
+  club_parent_id: string;
+  qrcode_id: string;
+  index: number;
+}
 
 export const updateQRCode = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => { 
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const clubName: string = req.body.club_name;
 
+  const genID = uniqid();
+  
+  const metaArr: metaData[] = [];
 
-    const metaArr: metaData[] = [];
-    const genID = uniqid()
-    const userSpreadSheetID: any = process.env.USER_DATA_SPREADSHEET_ID;
-    const metaDataSheetID: string =
-      "1vCC8ercuyn8ayszYSYEtpHFs0lxkLhxn0rF_IMyTU5E";
+  const metaDataSheetID: string =
+    "1MTpouiXD8gVt4nYxPqlBlTa_54XXJ3gup3WKMcTFK8A";
+  const metaDoc = new GoogleSpreadsheet(metaDataSheetID, serviceAccountAuth);
 
-    async function findClassID(clubName: string) {
-        const metaDoc = new GoogleSpreadsheet(metaDataSheetID, serviceAccountAuth);
-    
-        await metaDoc.loadInfo();
-      
-        const metaSheet = metaDoc.sheetsByIndex[0];
-        const metaRows = await metaSheet.getRows();
-        const metaSheetLen: number = metaSheet.rowCount;
-    
-        for (let i = 0; i < metaSheetLen; i++) {
-          if (metaRows[i] === undefined) {
-            break;
-          } else {
-        
-          const data: metaData = {
-              
-              club_name: metaRows[i].get("Club Name"),
-              club_parent_id: metaRows[i].get("Club Folder ID"),
-            };
-    
-            metaArr.push(data);
-    
-            // console.log(user_rows[i].get("UID"))
-          }
-        }
-    
-        const ID = metaArr.filter(el => el.club_name === clubName );
-    
-        return ID
+  await metaDoc.loadInfo();
+
+  const metaSheet = metaDoc.sheetsByIndex[0];
+  const metaRows = await metaSheet.getRows();
+  const metaSheetLen: number = metaSheet.rowCount;
+
+  async function findClassID(clubName: string) {
+    for (let i = 0; i < metaSheetLen; i++) {
+      if (metaRows[i] === undefined) {
+        break;
+      } else {
+        const data: metaData = {
+          club_name: metaRows[i].get("Club Name"),
+          club_parent_id: metaRows[i].get("Club Folder ID"),
+          qrcode_id: metaRows[i].get("QR Code"),
+          index: i,
+        };
+
+        metaArr.push(data);
+
+        // console.log(user_rows[i].get("UID"))
       }
-    
+    }
 
-    
-  async function createQRCode(parentID: string, folderName: string) {
+    const ID = metaArr.filter((el) => el.club_name === clubName);
+
+    return ID;
+  }
+
+  async function updateQRCode(folderName: string) {
     try {
+      const classIDS = await findClassID(folderName);
+      const qrcodeID = classIDS[0].qrcode_id;
+      const qrfile = await service.files.delete({
+        fileId: qrcodeID,
+      });
+
       const trimFolderName = folderName.replace(/ /g, "_");
-      console.log(trimFolderName)
+      console.log(trimFolderName);
       const link = `https://www.test.com/${trimFolderName}?id=${genID}`;
-      const qrcode = await QRCode.toFile( `./imgs/${trimFolderName}.png`, link, { type: "png"})
+      const qrcode = await QRCode.toFile(`${trimFolderName}.png`, link, {
+        type: "png",
+      });
 
-      const buffer = fs.readFileSync(`./imgs/${trimFolderName}.png`);
+      const buffer = fs.readFileSync(`${trimFolderName}.png`);
 
-      //Johnson please save the qrcode to the drive, the parent ID is here 
+      //Johnson please save the qrcode to the drive, the parent ID is here
       const file = await service.files.create({
         requestBody: {
-            name: `${folderName} QR Code`,
-            parents: [`${parentID}`],
-          },
-          media: {
-            mimeType: "image/png",
-            body: Readable.from([buffer]),
-          },
-    });
-      console.log('File Id:', file.data.id);
+          name: `${folderName} QR Code`,
+          parents: [`${classIDS[0].club_parent_id}`],
+        },
+        media: {
+          mimeType: "image/png",
+          body: Readable.from([buffer]),
+        },
+      });
 
-      return qrcode;
+      metaRows[classIDS[0].index].set("QR Code", file.data.id);
+      await metaRows[classIDS[0].index].save();
+
+      console.log("File Id:", file.data.id);
     } catch (error) {
       console.error(error);
     }
   }
 
   try {
-    const date = new Date().toLocaleDateString()
+    await updateQRCode(clubName);
 
-    res.json(date)
-  } catch (error) {
-    
-  }
-
-  }
+    res.json(genID);
+  } catch (error) {}
+};
