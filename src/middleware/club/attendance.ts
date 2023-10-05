80ef1ca7ca69092e7db85f56e935a349daf75dd6
Aiden8641 <90789887+Aiden8641@users.noreply.github.com>
2023-10-05 11:04:19 -0400

    Revert "Merge remote-tracking branch 'origin/ryan-branch' into dev"
    This reverts commit a42c868b0880eb6dd9d3522dc526ed5c2eadc67d, reversing
    changes made to 89301cf58b1d8d7213eb87f92520c126dc39548d.

--- src/middleware/club/attendance.ts
+++ src/middleware/club/attendance.ts
@@ -2,12 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { serviceAccountAuth, service } from "../../app";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import { getClubSheet } from "./clubData";
-import {
-  attendanceData,
-  dateData,
-  metaData,
-  clubData,
-} from "../../interface/interface";
+import { attendanceData, dateData, metaData } from "../../interface/interface";
 
 export const updateAttendance = async (
   req: Request,
@@ -16,25 +11,17 @@ export const updateAttendance = async (
 ) => {
   const date = new Date().toLocaleDateString();
   const data = req.body as attendanceData;
-  const clubData = req.body as clubData;
-
   const arrUID: string[] = [];
   const dateArr: dateData[] = [];
   const metaArr: metaData[] = [];
   const attendanceArrUID: string[] = [];
-  const masterArr: string[] = [];
 
   //ENVIRONMENT VARIABLES
   const userSpreadSheetID = process.env.USER_DATA_SPREADSHEET_ID as string;
   const metaDataSheetID = process.env.META_DATA_SPREADSHEET_ID as string;
-  const masterSpreadSheetID = process.env.MASTER_SPREADSHEET_ID as string;
 
   //GET USER INFORMATION
   const userDoc = new GoogleSpreadsheet(userSpreadSheetID, serviceAccountAuth);
-  const masterDoc = new GoogleSpreadsheet(
-    masterSpreadSheetID,
-    serviceAccountAuth
-  );
 
   let headerValues: string[] = [
     "UID",
@@ -50,6 +37,7 @@ export const updateAttendance = async (
   //FROM META DATA SHEET GET CLUB SHEET ID AND NAME
   async function findClassID(clubName: string) {
     const metaDoc = new GoogleSpreadsheet(metaDataSheetID, serviceAccountAuth);
+
     await metaDoc.loadInfo();
 
     const metaSheet = metaDoc.sheetsByIndex[0];
@@ -79,18 +67,12 @@ export const updateAttendance = async (
   // const x = user_rows[0].get('UID')
   // console.log(x)
 
-  async function updateAttendance(
-    uid: string,
-    clubName: string,
-    room: string,
-    firstName: string,
-    lastName: string
-  ) {
+  async function updateAttendance(uid: string, clubName: string) {
     const arrAttendanceID = await findClassID(clubName);
     let attendanceID: string = "";
 
     if (arrAttendanceID === undefined) {
-      return res.json("Invalid class name");
+      return res.json("invalid class name");
     } else {
       attendanceID = arrAttendanceID[0].club_spreadsheet_id;
     }
@@ -129,18 +111,11 @@ export const updateAttendance = async (
     //END OF CREATING SHEETS
 
     /* const attendanceSheet = attendanceDoc.sheetsByIndex[0]; */
-    await masterDoc.loadInfo();
-
     const userSheet = userDoc.sheetsByIndex[0];
-    const masterSheet = masterDoc.sheetsByIndex[0];
-
     const userSheetLen = userSheet.rowCount;
     const attendanceSheetLen = newSheet.rowCount;
     const userRows = await userSheet.getRows();
     const attendanceRows = await newSheet.getRows();
-
-    const masterSheetLen = masterSheet.rowCount;
-    const masterRows = await masterSheet.getRows();
     // await attendance_sheet.loadCells("A1:K1");
     // const uid: number =
     //gets all users
@@ -167,10 +142,7 @@ export const updateAttendance = async (
     }
     const rowNum: number = attendanceArrUID.indexOf(uid);
 
-    console.log(attendanceArrUID);
-    console.log(rowNum);
-    console.log(masterSheet.title);
-
+    //MATCH USER INFO FROM USER SPREAD SHEET WITH THE UID
     if (arrUID.includes(uid)) {
       const userUID = attendanceArrUID.includes(uid);
       //IF USER IS NOT IN THE ATTENDANCE SHEET
@@ -187,32 +159,9 @@ export const updateAttendance = async (
           Date: date,
         });
 
-        if (masterSheet.title != date) {
-          await masterSheet.clearRows({ start: 2 });
-          await masterSheet.updateProperties({
-            title: `${date}`,
-          });
-
-          const updateMaster = await masterSheet.addRow({
-            Club: data.club_name,
-            Room: clubData.room,
-            Last: data.last_name,
-            First: data.first_name,
-          });
-          console.log("cleared rows and attendance updated (first signin)");
-        } else {
-          const updateMaster = await masterSheet.addRow({
-            Club: data.club_name,
-            Room: clubData.room,
-            Last: data.last_name,
-            First: data.first_name,
-          });
-          console.log("user attendance updated");
-        }
-
-        res.json("Attendance has been updated.");
+        res.json("added user to club attendance");
       } else if (attendanceRows[rowNum].get("Date") === date) {
-        res.json("Attendance may only be updated once a day.");
+        res.json("You may only update attendance once a day");
         console.log(attendanceRows[rowNum].get("Date"), date);
       } else {
         const attNum: string = attendanceRows[rowNum].get("# of Attendances");
@@ -220,22 +169,15 @@ export const updateAttendance = async (
         attendanceRows[rowNum].set("# of Attendances", turnNum + 1);
         attendanceRows[rowNum].set("Date", date);
         await attendanceRows[rowNum].save();
-        await masterRows[rowNum].save();
-        res.json(`Attendance updated: ${attNum} times`);
+        res.json(`updated attendance: ${attNum} `);
       }
     } else {
-      res.json("Please provide a valid UID.");
+      res.json("use a valid uid");
     }
   }
 
   try {
-    await updateAttendance(
-      data.uuid,
-      data.club_name,
-      clubData.room,
-      data.first_name,
-      data.last_name
-    );
+    await updateAttendance(data.uuid, data.club_name);
   } catch (error) {
     res.json(error);
   }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -49,6 +49,8 @@ router.post("/createClubTemplate", verifyAdmin, createClubTemplate);
 router.post("/uploadImage", upload.array("image"), uploadImage);
 router.post("/addClubMeeting", addClubMeeting);
 router.post("/addClub", verifyAdmin, addClubData);
+
+router.patch("/updateAttendance", updateAttendance);
 // router.patch("/changeMeta", changeMeta)
 
 router.delete("/deleteClubMeeting", deleteClubMeeting);
