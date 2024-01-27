4ff07e4ae91e4b0a58ab6ad796f157b67f67b674
Aiden Pang <aiden18288@gmail.com>
2024-01-27 14:28:50 -0500

    cleaned up verification

--- src/middleware/user/verification.ts
+++ src/middleware/user/verification.ts
@@ -1,34 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-import { serviceAccountAuth, service } from "../../app";
-import { GoogleSpreadsheet, GoogleSpreadsheetCell } from "google-spreadsheet";
-import { authority } from "../../enums/authority";
-
-export const verifyAdmin = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const userDoc = new GoogleSpreadsheet(
-    process.env.USER_DATA_SPREADSHEET_ID as string,
-    serviceAccountAuth
-  );
-  await userDoc.loadInfo();
-  const userSheet = userDoc.sheetsByIndex[0];
-  const userRows = await userSheet.getRows();
-
-  const admin = userRows.filter(
-    (user) =>
-      user.get("Client Authority") === "admin" &&
-      user.get("Email") === req.body.email
-  );
-  console.log(admin);
-
-  if (admin.length === 0) {
-    res.json({ message: "You are not an admin!" });
-  } else {
-    return next();
-  }
-};
+import { serviceAccountAuth } from "../../app";
+import { GoogleSpreadsheet } from "google-spreadsheet";
 
 /**
  * Middleware function to verify user authority.
