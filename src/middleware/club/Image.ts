c907460a8a75fb3c2f709f99760ac90d94ab058b
Aiden Pang <90789887+Aiden8641@users.noreply.github.com>
2024-05-04 16:28:27 -0400

    some tests for images going to make it better later

--- src/middleware/club/Image.ts
+++ src/middleware/club/Image.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { service } from "../../app";
 import { Readable } from "stream";
 import {
-  findMeta_ParentFolder,
-  getMetaSheet,
+	findMeta_ParentFolder,
+	getMetaSheet,
 } from "../Folder_Meta_Utils/FindMeta_ParentFolder";
 
 /**
@@ -13,45 +13,45 @@ import {
  * @param next The next middleware function in the request-response cycle.
  */
 export const uploadImage = async (req: Request, res: Response) => {
-  try {
-    const { year, clubName } = req.body;
-    const photoFolderId = process.env.CLUB_IMAGE_FOLDER_ID as string;
+	try {
+		const { year, clubName } = req.body;
+		const photoFolderId = process.env.CLUB_IMAGE_FOLDER_ID as string;
 
-    if (!year || !clubName || !photoFolderId) {
-      res.status(400).json("Missing required parameters");
-    }
+		if (!year || !clubName || !photoFolderId) {
+			res.status(400).json("Missing required parameters");
+		}
 
-    const files = req.files;
+		const files = req.files;
 
-    if (!files || files.length === 0) {
-      res.json("No files to upload");
-    }
+		if (!files || files.length === 0) {
+			res.json("No files to upload");
+		}
 
-    const uploadPromises = files.map(async (file: any) => {
-      const response = await service.files.create({
-        requestBody: {
-          name: clubName,
-          parents: [photoFolderId],
-          appProperties: {
-            year: year,
-          },
-        },
-        media: {
-          mimeType: file.mimetype,
-          body: Readable.from([file.buffer]),
-        },
-        fields: "id",
-      });
+		const uploadPromises = files.map(async (file: any) => {
+			const response = await service.files.create({
+				requestBody: {
+					name: clubName,
+					parents: [photoFolderId],
+					appProperties: {
+						year: year,
+					},
+				},
+				media: {
+					mimeType: file.mimetype,
+					body: Readable.from([file.buffer]),
+				},
+				fields: "id",
+			});
 
-      console.log("File Id:", response.data.id);
-    });
+			console.log("File Id:", response.data.id);
+		});
 
-    await Promise.all(uploadPromises);
+		await Promise.all(uploadPromises);
 
-    res.status(200).json({ message: "File uploaded successfully!" });
-  } catch (err) {
-    res.json(err);
-  }
+		res.status(200).json({ message: "File uploaded successfully!" });
+	} catch (err) {
+		res.json(err);
+	}
 };
 
 /**
@@ -60,66 +60,66 @@ export const uploadImage = async (req: Request, res: Response) => {
  * @param res - The response object used to send the result of the function.
  */
 export const approveImage = async (req: Request, res: Response) => {
-  try {
-    const { year, clubName } = req.body;
+	try {
+		const { year, clubName } = req.body;
 
-    if (!year || !clubName) {
-      res.json(400).json("Missing required parameters");
-    }
-    // Retrieve the metadata sheet ID for the given year
-    const metaSheetId = await findMeta_ParentFolder(year);
+		if (!year || !clubName) {
+			res.json(400).json("Missing required parameters");
+		}
+		// Retrieve the metadata sheet ID for the given year
+		const metaSheetId = await findMeta_ParentFolder(year);
 
-    if (!metaSheetId) {
-      return res.status(404).json("Folder not found!");
-    }
+		if (!metaSheetId) {
+			return res.status(404).json("Folder not found!");
+		}
 
-    // Retrieve the metadata sheet for the specified club
-    const metaSheet = await getMetaSheet(
-      metaSheetId["Meta Sheet ID"],
-      clubName
-    );
+		// Retrieve the metadata sheet for the specified club
+		const metaSheet = await getMetaSheet(
+			metaSheetId["Meta Sheet ID"],
+			clubName
+		);
 
-    if (!metaSheet) {
-      res.status(404).json("Meta Not Found!");
-    }
+		if (!metaSheet) {
+			res.status(404).json("Meta Not Found!");
+		}
 
-    const query = `name = '${clubName}' and '${process.env.CLUB_IMAGE_FOLDER_ID}' in parents and appProperties has { key='year' and value='${year}' }`;
+		const query = `name = '${clubName}' and '${process.env.CLUB_IMAGE_FOLDER_ID}' in parents and appProperties has { key='year' and value='${year}' }`;
 
-    const listImgs = await service.files.list({
-      q: query,
-      fields: "files(id, name, parents, thumbnailLink)",
-      spaces: "drive",
-    });
+		const listImgs = await service.files.list({
+			q: query,
+			fields: "files(id, name, parents, thumbnailLink)",
+			spaces: "drive",
+		});
 
-    if (listImgs.data.files.length === 0) {
-      return res.status(404).json("No images found!");
-    }
+		if (listImgs.data.files.length === 0) {
+			return res.status(404).json("No images found!");
+		}
 
-    const imageId = listImgs.data.files[0].id as string;
-    const currentParent = listImgs.data.files[0].parents[0] as string;
-    const newParent = metaSheet.get("Club Photo Folder ID") as string;
+		const imageId = listImgs.data.files[0].id as string;
+		const currentParent = listImgs.data.files[0].parents[0] as string;
+		const newParent = metaSheet.get("Club Photo Folder ID") as string;
 
-    // add drive thumbnail link to meta
-    metaSheet.set(
-      "Club Attendance Photo",
-      listImgs.data.files[0].thumbnailLink
-    );
+		// add drive thumbnail link to meta
+		metaSheet.set(
+			"Club Attendance Photo",
+			listImgs.data.files[0].thumbnailLink
+		);
 
-    await metaSheet.save();
+		await metaSheet.save();
 
-    await service.files.update({
-      fileId: imageId,
-      addParents: newParent,
-      removeParents: currentParent,
-      fields: "id, parents",
-    });
+		await service.files.update({
+			fileId: imageId,
+			addParents: newParent,
+			removeParents: currentParent,
+			fields: "id, parents",
+		});
 
-    res
-      .status(200)
-      .json("Image has been approved and has been moved to the new folder");
-  } catch (error) {
-    console.log(error);
-  }
+		res
+			.status(200)
+			.json("Image has been approved and has been moved to the new folder");
+	} catch (error) {
+		console.log(error);
+	}
 };
 /**
  * Retrieves a list of unapproved images from a Google Drive folder and sends the data as a JSON response.
@@ -127,37 +127,41 @@ export const approveImage = async (req: Request, res: Response) => {
  * @param res - The response object from Express.
  */
 export const getUnapprovedImage = async (req: Request, res: Response) => {
-  try {
-    const images = await service.files.list({
-      q: `'${process.env.CLUB_IMAGE_FOLDER_ID}' in parents`,
-      fields: "files(id, name, thumbnailLink)",
-      spaces: "drive",
-    });
-    res.status(200).json(images.data.files);
-  } catch (error) {
-    res.status(400).json(error);
-  }
+	try {
+		const images = await service.files.list({
+			q: `'${process.env.CLUB_IMAGE_FOLDER_ID}' in parents`,
+			fields: "files(id, name, thumbnailLink)",
+			spaces: "drive",
+		});
+		if (images.data.files.length > 0) {
+			res.status(200).json(images.data.files);
+		} else {
+			res.status(204).json("No Images to be Approved")
+		}
+	} catch (error) {
+		res.status(400).json(error);
+	}
 };
 
 export const unapprovedImage = async (req: Request, res: Response) => {
-  try {
-    const { imageId } = req.body;
+	try {
+		const { imageId } = req.body;
 
-    if (!imageId) {
-      res.status(400).json("Missing required parameters");
-    }
+		if (!imageId) {
+			res.status(400).json("Missing required parameters");
+		}
 
-    const image = await service.files.delete({
-      fileId: imageId,
-      fields: "files(id)",
-    });
+		const image = await service.files.delete({
+			fileId: imageId,
+			fields: "files(id)",
+		});
 
-    console.log(image.data);
+		console.log(image.data);
 
-    res
-      .status(200)
-      .json("Image has been unapproved and has been deleted from the folder");
-  } catch (error) {
-    console.log(error);
-  }
+		res
+			.status(200)
+			.json("Image has been unapproved and has been deleted from the folder");
+	} catch (error) {
+		console.log(error);
+	}
 };
--- src/test/image.test.ts
+++ src/test/image.test.ts
@@ -11,27 +11,101 @@ import path = require("path");
 // image
 // upload image
 request(app)
-  .post("/uploadImage")
-  .attach("image", path.resolve(__dirname, "../../3d+rapid+prototyping.jpeg"))
-  .field("year", "2024-2025")
-  .field("clubName", "Anime Club")
-  .field("uuid", "113380945040354412648")
-  .expect(200)
-  .expect({ message: "File uploaded successfully!" })
-  .end(function (err, res) {
-    if (err) {
-      console.log(err);
-    }
-  });
+	.post("/uploadImage")
+	.attach("image", path.resolve(__dirname, "../../3d+rapid+prototyping.jpeg"))
+	.field("year", "2024-2025")
+	.field("clubName", "Anime Club")
+	.field("uuid", "113380945040354412648")
+	.expect(200)
+	.expect({ message: "File uploaded successfully!" })
+	.end(function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+
+request(app).post("/uploadImage")
+	.expect(400)
+	.expect("Missing required parameters")
+	.end(function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+// Get Unapproved images
+// need to pass uuid in before running test
 request(app)
-  .post("/uploadImage")
-  .expect(400)
-  .expect("Missing required parameters")
-  .end(function (err, res) {
-    if (err) {
-      console.log(err);
-    }
-  });
-// 
+	.get("/getUnapprovedImages/:uuid")
+	.expect(200 || 204)
+	.expect(
+		function(res) {
+			Array.isArray(res.body);
+		} || "No Images to be Approved"
+	);
+
 // approve image
-request(app).patch("/approveImage").
+request(app)
+	.patch("/approveImage")
+	.field("year", "2024-2025")
+	.field("clubName", "Anime Club")
+	.expect(200)
+	.expect("Image has been approved and has been moved to the new folder")
+	.end((err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+
+request(app)
+	.patch("/approveImage")
+	.expect(400)
+	.expect("Missing required parameters")
+	.end((err) => {
+		console.log(err);
+	});
+
+request(app)
+	.patch("/approveImage")
+	.field("year", "0000-0000")
+	.field("clubName", "Anime Club")
+	.expect(404)
+	.expect("Folder not found!")
+	.end((err) => {
+		console.log(err);
+	});
+
+request(app)
+	.patch("/approveImage")
+	.field("year", "2024-2025")
+	.field("clubName", "ClubDoesNotExist")
+	.expect(404)
+	.expect("Meta Not Found!")
+	.end((err) => {
+		console.error(err);
+	});
+
+// Image should have already be approved their should be no other image with these fields
+request(app)
+	.patch("/approveImage")
+	.field("year", "2024-2025")
+	.field("clubName", "Anime Club")
+	.expect(404)
+	.expect("No images found!")
+	.end((err) => {
+		console.error(err);
+	});
+// probably need to create the image before unapproving it
+request(app)
+	.patch("/unapproveImage")
+	.field("imageId", "id")
+	.expect(200, "Image has been unapproved and has been deleted from the folder")
+	.end((err) => {
+		console.error(err);
+	});
+
+request(app)
+	.patch("/unapproveImage")
+	.expect(400, "Missing required parameters")
+	.end((err) => {
+		console.error(err);
+	});
